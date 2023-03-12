6d81991be3a2075a460f503f135915eaf3aacbc7
NutanpramodDeshmukh <nutanpd17@gmail.com>
2023-03-12 15:05:32 +0530

    list product Reviews

--- ProductReviewManagement/Program.cs
+++ ProductReviewManagement/Program.cs
@@ -9,10 +9,19 @@ namespace ProductReviewManagement
         {
             List<ProductReview> ProductReviewlist = new List<ProductReview>()
             {
-                new ProductReview() {ProductId=1,UserId=1,Rating=5,Review="Good",isLike=true},
+                new ProductReview() { ProductId = 1, UserId = 1, Rating = 5, Review = "Good", isLike = true},
                 new ProductReview() { ProductId = 2, UserId = 2, Rating = 4.5, Review = "Good", isLike = true },
                 new ProductReview() { ProductId = 3, UserId = 3, Rating = 4, Review = "Good", isLike = true },
-                new ProductReview() { ProductId = 4, UserId = 3, Rating = 3.9, Review = "Good", isLike = true }
+                new ProductReview() { ProductId = 4, UserId = 4, Rating = 3.5, Review = "not Good", isLike = false },
+                new ProductReview() { ProductId = 5, UserId = 5, Rating = 4.8, Review = "Good", isLike = true },
+                new ProductReview() { ProductId = 2, UserId = 6, Rating = 4.1, Review = "Good", isLike = true },
+                new ProductReview() { ProductId = 7, UserId = 7, Rating = 3.6, Review = "not Good", isLike = false },
+                new ProductReview() { ProductId = 8, UserId = 8, Rating = 4.3, Review = "Good", isLike = true },
+                new ProductReview() { ProductId = 9, UserId = 9, Rating = 3.7, Review = "Good", isLike = true },
+                new ProductReview() { ProductId = 10, UserId = 10, Rating = 4.4, Review = "Good", isLike = true },
+                new ProductReview() { ProductId = 5, UserId = 8, Rating = 4.6, Review = "Good", isLike = true },
+                new ProductReview() { ProductId = 4, UserId = 9, Rating = 4.7, Review = "Good", isLike = true },
+                new ProductReview() { ProductId = 8, UserId = 10, Rating = 4.8, Review = "Good", isLike = true }
             };
             foreach (ProductReview Pr in ProductReviewlist)
             {
--- ProductReviewManagement/Records.cs
+++ ProductReviewManagement/Records.cs
@@ -18,12 +18,12 @@ namespace ProductReviewManagement
                 Console.WriteLine($"ProductId: {Pr.ProductId} UserId: {Pr.ProductId} Rating: {Pr.Rating} Review: {Pr.Review} isLike: {Pr.isLike}");
             }
         }
-        public void FilterRecord(List<ProductReview> listproductReviews)
+        public void RetriveCountOfRecords(List<ProductReview> listproductReviews)
         {
-            var recordedData = from ProductReviews in listproductReviews where (ProductReviews.ProductId == 1 || ProductReviews.ProductId == 4 || ProductReviews.ProductId == 9) && ProductReviews.Rating > 3 select ProductReviews;
+            var recordedData = listproductReviews.GroupBy(x => x.ProductId).Select(x => new { productId = x.Key, count = x.Count() });
             foreach (var Pr in recordedData)
             {
-                Console.WriteLine($"ProductId: {Pr.ProductId} UserId: {Pr.ProductId} Rating: {Pr.Rating} Review: {Pr.Review} isLike: {Pr.isLike}");
+                Console.WriteLine($"ProductId: {Pr.productId}   Count: {Pr.count}");
             }
         }
     }
