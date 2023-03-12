f10d5f1c9761bdd7055080da3df08695620c903f
NutanpramodDeshmukh <nutanpd17@gmail.com>
2023-03-12 14:39:40 +0530

    initial message

---
+++ ProductReviewManagement/ProductReview.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    public class ProductReview
+    {
+        public int ProductId;
+        public int UserId;
+        public double Rating;
+        public string Review;
+        public bool isLike;
+    }
+}
---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductReviewManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ProductReview> ProductReviewlist = new List<ProductReview>()
+            {
+                new ProductReview() {ProductId=1,UserId=1,Rating=5,Review="Good",isLike=true},
+                new ProductReview() { ProductId = 2, UserId = 2, Rating = 4.5, Review = "Good", isLike = true },
+                new ProductReview() { ProductId = 3, UserId = 3, Rating = 4, Review = "Good", isLike = true },
+                new ProductReview() { ProductId = 4, UserId = 3, Rating = 3.9, Review = "Good", isLike = true }
+            };
+            foreach (ProductReview Pr in ProductReviewlist)
+            {
+                Console.WriteLine($"ProductId: {Pr.ProductId} UserId: {Pr.ProductId} Rating: {Pr.Rating} Review: {Pr.Review} isLike: {Pr.isLike}");
+            }
+        }
+    }
+}
