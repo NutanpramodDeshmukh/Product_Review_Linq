a09efe01af8ae5880186b2411b311e8958075db4
NutanpramodDeshmukh <nutanpd17@gmail.com>
2023-03-12 14:49:10 +0530

    top product Reviews

---
+++ ProductReviewManagement/Records.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    public class Records
+    {
+        public void TopRecords(List<ProductReview> listproductReviews)
+        {
+            var recordedData = (from productReviews in listproductReviews
+                                orderby productReviews.Rating descending
+                                select productReviews).Take(3);
+            foreach (var Pr in recordedData)
+            {
+                Console.WriteLine($"ProductId: {Pr.ProductId} UserId: {Pr.ProductId} Rating: {Pr.Rating} Review: {Pr.Review} isLike: {Pr.isLike}");
+            }
+        }
+    }
+}
