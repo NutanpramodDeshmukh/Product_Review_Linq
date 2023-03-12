8b518762ad32940c360710791e07434cf3069304
NutanpramodDeshmukh <nutanpd17@gmail.com>
2023-03-12 14:56:19 +0530

    greater Reviews

--- ProductReviewManagement/Records.cs
+++ ProductReviewManagement/Records.cs
@@ -18,5 +18,13 @@ namespace ProductReviewManagement
                 Console.WriteLine($"ProductId: {Pr.ProductId} UserId: {Pr.ProductId} Rating: {Pr.Rating} Review: {Pr.Review} isLike: {Pr.isLike}");
             }
         }
+        public void FilterRecord(List<ProductReview> listproductReviews)
+        {
+            var recordedData = from ProductReviews in listproductReviews where (ProductReviews.ProductId == 1 || ProductReviews.ProductId == 4 || ProductReviews.ProductId == 9) && ProductReviews.Rating > 3 select ProductReviews;
+            foreach (var Pr in recordedData)
+            {
+                Console.WriteLine($"ProductId: {Pr.ProductId} UserId: {Pr.ProductId} Rating: {Pr.Rating} Review: {Pr.Review} isLike: {Pr.isLike}");
+            }
+        }
     }
 }
